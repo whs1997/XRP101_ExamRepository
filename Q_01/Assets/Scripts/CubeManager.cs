fc006c632c9ffd2e3e41817c441e9257fd94ead4
whs1997 <whs1997@naver.com>
2024-11-19 11:03:50 +0900

    Fix: 메서드 위치 이동
    큐브 생성 확인

--- Q_01/Assets/Scripts/CubeManager.cs
+++ Q_01/Assets/Scripts/CubeManager.cs
@@ -11,12 +11,17 @@ public class CubeManager : MonoBehaviour
 
     private void Awake()
     {
-        SetCubePosition(3, 0, 3);
+        // SetCubePosition(3, 0, 3);
     }
 
     private void Start()
     {
+        // 큐브컨트롤러를 가진 큐브 프리팹이 생성되기 전에 SetCubePosition을 불러와서
+        // 생성되지 않은 큐브를 참조해 NullReferenceException 에러가 뜬다.
+        // SetCubePosition을 CreateCube 뒤로 이동시켜 큐브가 생기는걸 확인했다.
         CreateCube();
+
+        SetCubePosition(3, 0, 3);
     }
 
     private void SetCubePosition(float x, float y, float z)
